ec6ef2949f840a0aed3fe77e7e5b6ca9479f0251
michwong98 <michwong98@gmail.com>
2020-05-26 19:46:03 -0400

    Add dependency injection for IBusinessService

--- project2/CharSheet/CharSheet.Api/Startup.cs
+++ project2/CharSheet/CharSheet.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using CharSheet.Api.Services;
 
 namespace CharSheet.Api
 {
@@ -27,6 +28,8 @@ namespace CharSheet.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<IBusinessService, BusinessService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
