c91f3200b39195deb4ff12981759d6d69f327b4d
michwong98 <michwong98@gmail.com>
2020-05-27 12:43:07 -0400

    PUT for Sheets

---
+++ project2/CharSheet/CharSheet.Data/Repositories/FormInputRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using CharSheet.Domain;
+using CharSheet.Domain.Interfaces;
+
+namespace CharSheet.Data.Repositories
+{
+    public class FormInputRepository : GenericRepository<FormInput>, IFormInputRepository
+    {
+        public FormInputRepository(CharSheetContext context)
+            : base(context)
+        { }
+
+        public async Task<IEnumerable<FormInput>> RemoveRange(IEnumerable<FormInput> formInputs)
+        {
+            foreach(var formInput in formInputs)
+            {
+                await this.Remove(formInput);
+            }
+            return formInputs;
+        }
+    }
+}
