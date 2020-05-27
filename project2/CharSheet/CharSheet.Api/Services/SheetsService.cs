78fdcc6de1f0f2232638606ded7e4f501c6e7b56
michwong98 <michwong98@gmail.com>
2020-05-27 10:39:38 -0400

    ToObject method for sheets

--- project2/CharSheet/CharSheet.Api/Services/SheetsService.cs
+++ project2/CharSheet/CharSheet.Api/Services/SheetsService.cs
@@ -16,8 +16,6 @@ namespace CharSheet.Api.Services
         #region GET
         Task<IEnumerable<SheetModel>> GetSheets(object id);
         Task<SheetModel> GetSheet(object id);
-        Task<FormInputGroupModel> GetFormInputGroup(FormInputGroup formInputGroup);
-        Task<FormInputGroupModel> GetFormInputGroup(object id);
         #endregion
 
         #region POST
@@ -54,33 +52,6 @@ namespace CharSheet.Api.Services
             // Returns sheet as sheet model.
             return await ToModel(sheet);
         }
-
-        public async Task<FormInputGroupModel> GetFormInputGroup(FormInputGroup formInputGroup)
-        {
-            // Instantiate form input group model.
-            var id = formInputGroup.FormInputGroupId;
-            var formInputGroupModel = new FormInputGroupModel
-            {
-                FormInputGroupId = formInputGroup.FormInputGroupId,
-
-                // Get form template as model.
-                FormTemplate = await this.GetFormTemplate(formInputGroup.FormTemplateId)
-            };
-
-            // Get form inputs as model.
-            var formInputs = await _unitOfWork.FormInputGroupRepository.GetFormInputs(id);
-            formInputGroupModel.FormInputs = formInputs.Select(formInput => formInput.Value);
-            return formInputGroupModel;
-        }
-
-        public async Task<FormInputGroupModel> GetFormInputGroup(object id)
-        {
-            // Load form input group from database.
-            var formInputGroup = await _unitOfWork.FormInputGroupRepository.Find(id);
-            if (formInputGroup == null)
-                throw new InvalidOperationException("Form input group not found.");
-            return await GetFormInputGroup(formInputGroup);
-        }
         #endregion
 
         #region POST
@@ -100,7 +71,7 @@ namespace CharSheet.Api.Services
                 var formTemplate = await _unitOfWork.FormTemplateRepository.Find(formInputGroupModel.FormTemplateId);
                 if (formTemplate == null)
                     throw new InvalidOperationException("Form template not found.");
-                
+
                 var formInputGroup = new FormInputGroup
                 {
                     FormTemplate = formTemplate,
@@ -129,6 +100,33 @@ namespace CharSheet.Api.Services
         #endregion
 
         #region Helpers
+        public async Task<FormInputGroupModel> GetFormInputGroup(FormInputGroup formInputGroup)
+        {
+            // Instantiate form input group model.
+            var id = formInputGroup.FormInputGroupId;
+            var formInputGroupModel = new FormInputGroupModel
+            {
+                FormInputGroupId = formInputGroup.FormInputGroupId,
+
+                // Get form template as model.
+                FormTemplate = await this.GetFormTemplate(formInputGroup.FormTemplateId)
+            };
+
+            // Get form inputs as model.
+            var formInputs = await _unitOfWork.FormInputGroupRepository.GetFormInputs(id);
+            formInputGroupModel.FormInputs = formInputs.Select(formInput => formInput.Value);
+            return formInputGroupModel;
+        }
+
+        public async Task<FormInputGroupModel> GetFormInputGroup(object id)
+        {
+            // Load form input group from database.
+            var formInputGroup = await _unitOfWork.FormInputGroupRepository.Find(id);
+            if (formInputGroup == null)
+                throw new InvalidOperationException("Form input group not found.");
+            return await GetFormInputGroup(formInputGroup);
+        }
+
         public async Task<SheetModel> ToModel(Sheet sheet)
         {
             // Load form input groups.
@@ -152,6 +150,43 @@ namespace CharSheet.Api.Services
             sheetModel.FormGroups = formInputGroupModels.AsEnumerable();
 
             return sheetModel;
+        }
+
+        public async Task<Sheet> ToObject(SheetModel sheetModel)
+        {
+            var sheet = new Sheet
+            {
+                UserId = sheetModel.UserId,
+                FormInputGroups = new List<FormInputGroup>()
+            };
+
+            // Create form input groups.
+            foreach (var formInputGroupModel in sheetModel.FormGroups)
+            {
+                var formTemplate = await _unitOfWork.FormTemplateRepository.Find(formInputGroupModel.FormTemplateId);
+                if (formTemplate == null)
+                    throw new InvalidOperationException("Form template not found.");
+
+                var formInputGroup = new FormInputGroup
+                {
+                    FormTemplate = formTemplate,
+                    FormInputs = new List<FormInput>()
+                };
+
+                for (int i = 0; i < formInputGroupModel.FormInputs.Count(); i++)
+                {
+                    var formInput = new FormInput
+                    {
+                        Index = i,
+                        Value = formInputGroupModel.FormInputs.ElementAt(i)
+                    };
+                    formInputGroup.FormInputs.Add(formInput);
+                }
+
+                sheet.FormInputGroups.Add(formInputGroup);
+            }
+
+            return await Task.FromResult(sheet);
         }
         #endregion
     }
