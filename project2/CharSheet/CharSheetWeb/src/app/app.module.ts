e5802e8f30dd4ac8fcbbec097cfbfaf619ad4e41
agony84 <jjk198478@gmail.com>
2020-05-29 14:21:19 -0600

    adjusts angular tests

--- project2/CharSheet/CharSheetWeb/src/app/app.module.ts
+++ project2/CharSheet/CharSheetWeb/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Importing social login module and google login provider.
-import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from "angularx-social-login";
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider, AuthService } from "angularx-social-login";
 
 import { AppComponent } from './app.component';
 
