df6f44bba1f4ff9f7532b50856c950198ebeebf7
michwong98 <michwong98@gmail.com>
2020-06-01 17:22:47 -0400

    Business service tests

--- project2/CharSheetApi/CharSheet.Api/Models/FormInputGroupModel.cs
+++ project2/CharSheetApi/CharSheet.Api/Models/FormInputGroupModel.cs
@@ -5,7 +5,7 @@ namespace CharSheet.Api.Models
 {
     public class FormInputGroupModel
     {
-        public Guid? FormTemplateId { get; set; }
+        //public Guid? FormTemplateId { get; set; }
         public FormTemplateModel FormTemplate { get; set; }
         public IEnumerable<string> FormInputs { get; set; }
     }
---
+++ project2/CharSheetApi/CharSheet.Test/APITests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using CharSheet.Data;
+using CharSheet.Domain;
+using CharSheet.Api.Services;
+using CharSheet.Api.Models;
+
+namespace CharSheet.Test
+{
+    public partial class APITests : DbContextTests
+    {
+        public Guid InsertUser(DbContextOptions<CharSheetContext> options)
+        {
+            using (var context = GetContext(options))
+            {
+                var user = context.Users.Add(new User()).Entity;
+                context.SaveChanges();
+                return user.UserId;
+            }
+        }
+
+        public IBusinessService GetBusinessService(CharSheetContext context)
+        {
+            var logger = new Mock<ILogger<BusinessService>>();
+            return new BusinessService(logger.Object, context);
+        }
+
+        public async Task<TemplateModel> InsertTemplate(IBusinessService service, Guid id)
+        {
+            var templateModel = new TemplateModel
+            {
+                FormTemplates = new List<FormTemplateModel>
+                    {
+                        new FormTemplateModel
+                        {
+                            Labels = new List<string>()
+                        }
+                    }
+            };
+            return await service.CreateTemplate(templateModel, id);
+        }
+
+        public async Task<SheetModel> InsertSheet(IBusinessService service, Guid id)
+        {
+            var templateModel = await InsertTemplate(service, id);
+            var sheetModel = new SheetModel
+            {
+                FormGroups = new List<FormInputGroupModel>
+                {
+                    new FormInputGroupModel
+                    {
+                        FormTemplate = templateModel.FormTemplates.First(),
+                        FormInputs = new List<string>()
+                    }
+                }
+            };
+            return await service.CreateSheet(sheetModel, id);
+        }
+    }
+}
