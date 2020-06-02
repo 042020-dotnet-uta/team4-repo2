d7b3299157d614e083ab68706d912f3cbdd66a8a
AsraiLuna <brackenashleyb@gmail.com>
2020-06-02 18:05:26 -0400

    adds current state with workign API and div creation and locking

--- project2/CharSheet/CharSheetWeb/src/app/access-char-sheets/access-char-sheets.component.ts
+++ project2/CharSheet/CharSheetWeb/src/app/access-char-sheets/access-char-sheets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AccessCharSheetService } from "./access-char-sheet.service";
-import{HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 
 @Component({
@@ -9,12 +10,12 @@ import{HttpClient} from '@angular/common/http'
   styleUrls: ['./access-char-sheets.component.css']
 })
 export class AccessCharSheetsComponent implements OnInit {
-
   constructor(private http:HttpClient) { }
   httpData;
   ngOnInit(): void {
     this.http.get("http://jsonplaceholder.typicode.com/users")
-        .subscribe((data) => this.displayData(data));
+      .subscribe((data) => this.displayData(data));
   }
   displayData(data) { this.httpData = data; }
+  
 }
--- project2/CharSheet/CharSheetWeb/src/app/create-form/create-form.component.ts
+++ project2/CharSheet/CharSheetWeb/src/app/create-form/create-form.component.ts
@@ -19,46 +19,14 @@ var document;
 export class CreateFormComponent implements OnInit
 {
   formElements = [];
-  // create a style property to be worked with farther down 
-  style: { position: string; left: string; top: string; width: string; height: string; };
-
-  constructor(private draggable: DragDropModule) { }
-  //#region Validate resizing to prevent too little of a size
-  validate(event: ResizeEvent): boolean {
-    const minDimension: number = 50;
-    // checks to see if the rectangle width is less than the set minimum constraints
-    if (
-      event.rectangle.width &&
-        event.rectangle.height &&
-        (event.rectangle.width < minDimension ||
-          event.rectangle.height < minDimension)
-    ) {
-      // if the resized size is less than the minimum demension restraint return
-      // false as the result of the event
-      return false;
-    }
-    // if the size is larger than the minimum dimensions return true as the result of the event
-    return true;
-  }
-  // at the end of resizing  set the "style" positions
-  // Utilizing events to check when resizing is finished
-  // this should trigger when the resizing check returns true. 
-  onResizeEnd(event: ResizeEvent): void {
-    this.style = {
-      position: 'fixed',
-      left: `${event.rectangle.left}px`,
-      top: `${event.rectangle.top}px`,
-      width: `${event.rectangle.width}px`,
-      height: `${event.rectangle.height}px`
-    };
-  }
-  //endResizing Region
+  
   // create a method to be used in the HTML to  push a new dev of the predefined type
   // to the dom and to the page dynamically 
   createDragItem(): void {
     this.formElements.push(this.formElements.length);
     document.getElementById("form-boundary").appendChild(this.formElements);
   }
+
 
   ngOnInit(): void {}
 }
