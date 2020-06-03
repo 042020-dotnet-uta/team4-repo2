14d45bb053dba37aa99f8b3d0f3fae46c9c6f5a8
michwong98 <michwong98@gmail.com>
2020-06-03 16:42:02 -0400

    Change ModelState

--- project2/CharSheet/CharSheetWeb/src/app/api.service.ts
+++ project2/CharSheet/CharSheetWeb/src/app/api.service.ts
@@ -35,20 +35,21 @@ export class ApiService {
 
   public postTemplate(template: Template): Observable<any> {
     const headers = new HttpHeaders()
-    .set('Content-type', 'application/json')
-    .set('Authorization', `Bearer ${this.cookieService.get('apiToken')}`)
-    .set('Authenticationtoken', `Bearer ${this.cookieService.get('apiToken')}`)
-    .set('X-Requested-With', 'XMLHttpRequest');
-    return this.httpClient.post(this.connectionString + 'templates', template, { headers, observe: 'response' })
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${this.cookieService.get('apiToken')}`)
+      .set('Authenticationtoken', `Bearer ${this.cookieService.get('apiToken')}`)
+      .set('X-Requested-With', 'XMLHttpRequest');
+    return this.httpClient
+      .post<Template>(this.connectionString + 'templates', template, { headers, observe: 'response' });
   }
 
   public postSheet(sheet: Sheet): Observable<any> {
     const headers = new HttpHeaders()
-    .set('Content-type', 'application/json')
-    .set('Authorization', `Bearer ${this.cookieService.get('apiToken')}`)
-    .set('Authenticationtoken', `Bearer ${this.cookieService.get('apiToken')}`)
-    .set('X-Requested-With', 'XMLHttpRequest');
-    return this.httpClient.post(this.connectionString + 'sheets', sheet, {headers, observe: 'response'});
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${this.cookieService.get('apiToken')}`)
+      .set('Authenticationtoken', `Bearer ${this.cookieService.get('apiToken')}`)
+      .set('X-Requested-With', 'XMLHttpRequest');
+    return this.httpClient.post(this.connectionString + 'sheets', sheet, { headers, observe: 'response' });
   }
 }
 
@@ -81,8 +82,7 @@ export interface Sheet {
   sheetId: string;
   formGroups: FormGroup[];
 }
-export interface FormGroup
-{
+export interface FormGroup {
   formTemplate: FormTemplate;
   formInputs: string[];
 }
