8cff19fd2bf7b8e75999eee8b50a24d2d5b2b3a5
michwong98 <michwong98@gmail.com>
2020-06-03 12:31:11 -0400

    Google Auth token

--- project2/CharSheet/CharSheetWeb/src/app/api.service.ts
+++ project2/CharSheet/CharSheetWeb/src/app/api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
+import { SocialUser } from "angularx-social-login";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +13,12 @@ export class ApiService {
 
   public userLogin(login: Login): Observable<any> {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this.httpClient.post('https://revatureprojectapi.azurewebsites.net/api/account/login', login, {headers, responseType: 'text', observe: 'response' });
+    return this.httpClient.post('https://revatureprojectapi.azurewebsites.net/api/account/login', login, { headers, responseType: 'text', observe: 'response' });
+  }
+
+  public googleLogin(user: SocialUser): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.httpClient.post('https://revatureprojectapi.azurewebsites.net/api/account/googlelogin', { username: user.name, email: user.email }, { headers, responseType: 'text', observe: 'response' });
   }
 
   public register(register: Register): Observable<any> {
@@ -19,7 +26,7 @@ export class ApiService {
     return this.httpClient.post('https://revatureprojectapi.azurewebsites.net/api/account/register', register, { observe: 'response' });
   }
 
-  public getTemplate(templateId: string): Observable<object> {
+  public getTemplate(templateId: string): Observable<any> {
     return this.httpClient.get(`https://revatureprojectapi.azurewebsites.net/api/templates/${templateId}`, { observe: 'response' })
   }
 }
