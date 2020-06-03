5c429b9468d8196d126bacda8e4a5eee976f7eb9
agony84 <jjk198478@gmail.com>
2020-06-03 13:50:54 -0600

    fixes create-form component tests

--- project2/CharSheet/CharSheetWeb/src/app/create-form/create-form.component.spec.ts
+++ project2/CharSheet/CharSheetWeb/src/app/create-form/create-form.component.spec.ts
@@ -3,14 +3,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateFormComponent } from './create-form.component';
 //  import dependecies
 import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '../api.service';
 
 describe('CreateFormComponent', () => {
   let component: CreateFormComponent;
   let fixture: ComponentFixture<CreateFormComponent>;
+  let apiStub: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CreateFormComponent],
+      providers: [
+        { provide: ApiService, useValue: apiStub },
+      ],
       imports: [
         RouterTestingModule
       ]
