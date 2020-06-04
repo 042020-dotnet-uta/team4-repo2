e940581e0dee269009ecabd531983c86905a487d
agony84 <jjk198478@gmail.com>
2020-06-04 16:04:42 -0600

    updates print pdf api

--- project2/CharSheet/CharSheetWeb/src/app/selected-sheet/selected-sheet.component.ts
+++ project2/CharSheet/CharSheetWeb/src/app/selected-sheet/selected-sheet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SavePdfService } from '../print-pdf.service'
 
 @Component({
   selector: 'app-selected-sheet',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectedSheetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private savePdfService: SavePdfService) { }
 
   ngOnInit(): void {
+  }
+
+  saveToPdf(){
+    this.savePdfService.captureScreen();
   }
 
 }
