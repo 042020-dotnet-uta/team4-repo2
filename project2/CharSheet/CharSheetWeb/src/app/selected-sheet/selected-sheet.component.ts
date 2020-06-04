f07e4883b3be8e5b34851d85a4f737a6157eec52
michwong98 <michwong98@gmail.com>
2020-06-04 16:49:59 -0400

    Renamed file names and added sheet routing

---
+++ project2/CharSheet/CharSheetWeb/src/app/selected-sheet/selected-sheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-sheet',
+  templateUrl: './selected-sheet.component.html',
+  styleUrls: ['./selected-sheet.component.css']
+})
+export class SelectedSheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- project2/CharSheet/CharSheetWeb/src/app/selected-template/selected-template.component.spec.ts
+++ project2/CharSheet/CharSheetWeb/src/app/selected-template/selected-template.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SelectedCharSheetComponent } from './selected-char-sheet.component';
+import { SelectedTemplateComponent } from './selected-template.component';
 import { ApiService, Template, FormTemplate } from '../api.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-describe('SelectedCharSheetComponent', () => {
-  let component: SelectedCharSheetComponent;
-  let fixture: ComponentFixture<SelectedCharSheetComponent>;
+describe('SelectedTemplateComponent', () => {
+  let component: SelectedTemplateComponent;
+  let fixture: ComponentFixture<SelectedTemplateComponent>;
   let apiMock: any;
   let routeMock: any;
 
   beforeEach(async(() => {
     apiMock = jasmine.createSpyObj('ApiService', ['postTemplate']);
     TestBed.configureTestingModule({
-      declarations: [SelectedCharSheetComponent],
+      declarations: [SelectedTemplateComponent],
       providers: [
         { provide: ApiService, useValue: apiMock },
         {provide: ActivatedRoute, useValue: routeMock},
@@ -28,7 +28,7 @@ describe('SelectedCharSheetComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SelectedCharSheetComponent);
+    fixture = TestBed.createComponent(SelectedTemplateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
