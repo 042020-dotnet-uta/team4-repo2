224d6a4affa972fb9fbd1d69227f0696e313c150
agony84 <jjk198478@gmail.com>
2020-06-03 20:16:36 -0600

    more angular tests

--- project2/CharSheet/CharSheetWeb/src/app/generate-form.service.spec.ts
+++ project2/CharSheet/CharSheetWeb/src/app/generate-form.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GenerateFormService } from './generate-form.service';
+import { FormTemplate } from './api.service';
 
 describe('GenerateFormService', () => {
   let service: GenerateFormService;
+  let form: Partial<FormTemplate>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +14,15 @@ describe('GenerateFormService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getNewFormTemplate should return a FormTemplate', () => {
+    form = service.getNewFormTemplate();
+    expect(form).toBeTruthy();
+  });
+
+  it('getNewFormTemplate should return a FormTemplate with no title', () => {
+    form = service.getNewFormTemplate();
+    expect(form.title).toBe('');
   });
 });
