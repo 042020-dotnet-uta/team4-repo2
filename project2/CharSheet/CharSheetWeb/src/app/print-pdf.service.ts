a5e2f5e2cc752e8380eddbdcb61af52461f75670
agony84 <jjk198478@gmail.com>
2020-06-04 15:31:01 -0600

    adds print pdf function

---
+++ project2/CharSheet/CharSheetWeb/src/app/print-pdf.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SavePdfService {
+
+constructor() { }
+
+ // Method to capture form and convert to PDF
+  public captureScreen(){
+    var data = document.getElementById('form-boundary');
+    html2canvas(data).then(canvas=>{
+      let pdf = new jspdf('p', 'mm', [canvas.width, canvas.height]);
+      var imgData = canvas.toDataURL();
+      pdf.addImage(imgData, 0,0);
+      pdf.save('myCharSheet.pdf');
+      
+    });    
+  }  
+}
